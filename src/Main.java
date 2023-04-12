da8834cdc79d4a15210f4cbf4e462b763ebce5cf
aayushmandavya <mandavyaaayush98@gmail.com>
2023-04-12 10:36:22 +0545

    sdad

--- src/Main.java
+++ src/Main.java
@@ -21,9 +21,11 @@ public class Main {
         try {
             FileWriter  fileWriter = new FileWriter(file);
             System.out.println("File can be written successfully!");
-            fileWriter.write("Writing in a file\n");
-            fileWriter.write("Writing in a file\n");
-            fileWriter.write("Writing in a file\n");
+            fileWriter.write("Writing in a file 1\n");
+            fileWriter.write("Writing in a file 2\n");
+            fileWriter.write("Writing in a file 3\n");
+            fileWriter.write("Writing in a file 4\n");
+            fileWriter.write("Writing in a file 5\n");
 
             fileWriter.close();
             System.out.println("Written successfully in the file!");
@@ -31,13 +33,13 @@ public class Main {
             throw new RuntimeException(e);
         }
 //Reading from a file
-        try {
-            FileReader fileReader=new FileReader(file);
-            //until the value of file.read() value isn't -1
-            int character= fileReader.read();
-            while (character !=-1){
-                System.out.print((char)character);
-                character = fileReader.read();
+
+        try{
+            FileReader fileReader= new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String aayush;
+            while((aayush = bufferedReader.readLine()) != null){
+                System.out.println(aayush);
             }
 
         } catch (FileNotFoundException e) {
@@ -45,5 +47,19 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+//        try {
+//            FileReader fileReader=new FileReader(file);
+//            //until the value of file.read() value isn't -1
+//            int character= fileReader.read();
+//            while (character !=-1){
+//                System.out.print((char)character);
+//                character = fileReader.read();
+//            }
+//
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
